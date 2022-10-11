2c091466f4af371f69663d1d3d4743c985b2e904
Matthias Monnier <matthias.monnier@gmail.com>
2022-10-11 13:57:31 +0200

    Use getTokenData helper from sdk instead of item

--- souffl3_collec_test.ts
+++ souffl3_collec_test.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 import { AptosClient, AptosAccount, TokenClient } from 'aptos';
 import { NODE_URL, FAUCET_URL } from './common';
+import { exit } from 'process';
 
 interface NFT {
   collection: string;
@@ -15,9 +16,11 @@ interface NFT {
 (async () => {
   // Create API and faucet clients.
   const client = new AptosClient(NODE_URL);
+  // Create client for working with the token module.
+  const tokenClient = new TokenClient(client);
 
   const addressToScan =
-    '0x216b56a4241d72b6cd1e520bc840d51f3846d8366c8e4b130727e5dbc0a84057';
+    '0x81279b8c1419b0b750866ce4be7ecf8556e1e20acccada874f33e23676d15be2';
 
   // First let's get the deposed events
   const deposit_events = await client.getEventsByEventHandle(
@@ -42,17 +45,33 @@ interface NFT {
     });
     return true;
   });
+  //console.log(nft_owned);
 
   // Now let's fetch the URIs
   const nfts: NFT[] = [];
 
   const fetchAllUri = new Promise<void>((resolve, reject) => {
     nft_owned.forEach(async (e, index, arr) => {
+      /*const collectionData = await tokenClient.getCollectionData(
+        e.creator,
+        e.collection
+      );*/
+      //console.log(collectionData);
+
+      const tokenData = await tokenClient.getTokenData(
+        e.creator,
+        e.collection,
+        e.name
+      );
+      //console.log(tokenData);
+
+      /* Works but it's way easier with getTokenData helper from sdk
       const res = await client.getAccountResource(
         e.creator,
         '0x3::token::Collections'
       );
       const data = res.data as any;
+      console.log(data);
       const handle = data.token_data.handle;
 
       const item = await client.getTableItem(handle, {
@@ -63,9 +82,9 @@ interface NFT {
           collection: e.collection,
           name: e.name,
         },
-      });
+      });*/
 
-      nfts.push({ collection: e.collection, name: e.name, uri: item.uri });
+      nfts.push({ collection: e.collection, name: e.name, uri: tokenData.uri });
 
       if (index === arr.length - 1) resolve();
     });
